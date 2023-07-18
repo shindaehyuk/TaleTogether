ca341590ea7e7756a27f64becdd54de7dd43cff9
Kong-16 <alsgur97@naver.com>
2023-07-18 13:48:58 +0900

    delete backend directory

--- 공민혁/authTest/src/main/java/com/kong/backend/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kong.backend.user.repository;
-
-import com.kong.backend.user.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer>, UserRepositoryCustom{
-    User findUserById(String userId);
-    User findUserByName(String userName);
-}
