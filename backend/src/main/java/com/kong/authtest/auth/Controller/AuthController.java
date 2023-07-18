3698cd76b841bc827ed2d82e72988c3a284dcb34
Kwoo9 <rhks1132ek@naver.com>
2023-07-18 13:05:42 +0900

    Add backend dir & branch 백엔드 폴더로 생성

---
+++ backend/src/main/java/com/kong/authtest/auth/Controller/AuthController.java
@@ -0,0 +1,45 @@
+package com.kong.authtest.auth.Controller;
+
+import com.kong.authtest.auth.JwtTokenUtil;
+import com.kong.authtest.user.dto.UserDto;
+import com.kong.authtest.user.model.User;
+import com.kong.authtest.user.repository.UserRepository;
+import com.kong.authtest.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final PasswordEncoder passwordEncoder;
+    private final UserService userService;
+    private final JwtTokenUtil jwtTokenUtil;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody final UserDto userDto){
+        String userId = userDto.getUserId();
+        String password = userDto.getPassword();
+        User user = userService.getUserByUserId(userId);
+        if(passwordEncoder.matches(password,user.getPassword())){
+            Date date = new Date();
+            System.out.println(date.getTime());
+
+            final var ret = new HashMap<String, Object>();
+            ret.put("accessToken", jwtTokenUtil.generateAccessToken(userId));
+            ret.put("refreshToken", jwtTokenUtil.generateRefreshToken(userId));
+            return ResponseEntity.ok().body(ret);
+        }
+        return ResponseEntity.badRequest().build();
+    }
+}
