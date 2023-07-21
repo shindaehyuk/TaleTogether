d93828a9ed8c9eb55724653c0e269665ade8052d
Kwoo9 <rhks1132ek@naver.com>
2023-07-21 15:30:11 +0900

    Feat Swagger Setting about tale, comment, community, page

--- backend/src/main/java/com/kong/authtest/community/controller/CommunityController.java
+++ backend/src/main/java/com/kong/authtest/community/controller/CommunityController.java
@@ -5,6 +5,8 @@ import com.kong.authtest.community.dto.CommunityDtoRequest;
 import com.kong.authtest.community.dto.CommunityDtoResponse;
 import com.kong.authtest.community.dto.CommunityDtoPutRequest;
 import com.kong.authtest.community.service.CommunityService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.parameters.P;
@@ -14,26 +16,31 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 @CrossOrigin("*")
+@Api(tags = "community")
 public class CommunityController {
 
     private final CommunityService communityService;
 
     @PostMapping
+    @ApiOperation(value = "커뮤니티 작성 API", notes = "커뮤니티 글을 작성하기 위해 사용하는 API, title, content, userId, taleId가 필요하다", response = CommunityDtoResponse.class)
     public ResponseEntity<CommunityDtoResponse> register(@RequestBody CommunityDtoRequest communityDtoRequest){
         return ResponseEntity.ok(communityService.register(communityDtoRequest));
     }
 
     @GetMapping("/info/{communityId}")
+    @ApiOperation(value = "커뮤니티 정보 얻는 API", notes = "커뮤니티와 댓글 정보를 얻기위해 사용하는 API, communityId가 필요하다.", response = CommunityDtoGetResponse.class)
     public ResponseEntity<CommunityDtoGetResponse> getCommunityInfo(@PathVariable Long communityId){
         return ResponseEntity.ok(communityService.getCommunityInfo(communityId));
     }
 
     @PutMapping("/modify")
+    @ApiOperation(value = "커뮤니티 정보 수정 API", notes = "커뮤니티 정보를 수정하기위해 사용하는 API, taleId, content, title, communityId가 필요하다.", response = CommunityDtoResponse.class)
     public ResponseEntity<CommunityDtoResponse> modifyCommunityInfo(@RequestBody CommunityDtoPutRequest communityDtoPutRequest){
         return ResponseEntity.ok(communityService.modify(communityDtoPutRequest));
     }
 
     @DeleteMapping("/delete/{communityId}")
+    @ApiOperation(value = "커뮤니티 글을 삭제하기 위한 API", notes = "커뮤니티 글을 삭제하기 위한 API로 communityId가 필요하다. 커뮤니티에 달린 댓글들까지 같이 삭제된다. ", response = Boolean.class)
     public ResponseEntity<Boolean> delete(@PathVariable Long communityId){
         communityService.delete(communityId);
         return ResponseEntity.ok(true);
--- backend/src/main/java/com/kong/authtest/page/controller/PageController.java
+++ backend/src/main/java/com/kong/authtest/page/controller/PageController.java
@@ -4,6 +4,8 @@ import com.kong.authtest.page.dto.PageDtoPutRequest;
 import com.kong.authtest.page.dto.PageDtoRequest;
 import com.kong.authtest.page.dto.PageDtoResponse;
 import com.kong.authtest.page.service.PageService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -14,25 +16,30 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @CrossOrigin("*")
+@Api(tags = "page")
 public class PageController {
     private final PageService pageService;
 
     @PostMapping
+    @ApiOperation(value = "page를 작성하는 API", notes = "page를 작성하기 위한 API, content, image, taleId가 필요하다.", response = PageDtoResponse.class)
     public ResponseEntity<PageDtoResponse> register(@RequestBody PageDtoRequest PageDtoRequest) {
         return ResponseEntity.ok(pageService.register(PageDtoRequest));
     }
 
-    @GetMapping("/detail/{postId}")
-    public ResponseEntity<PageDtoResponse> detail(@PathVariable Long postId) {
-        return ResponseEntity.ok(pageService.detail(postId));
+    @GetMapping("/detail/{pageId}")
+    @ApiOperation(value = "page 정보를 얻기위한 API", notes = "page 정보를 얻기위한 API, pageId가 필요하다", response = PageDtoResponse.class)
+    public ResponseEntity<PageDtoResponse> detail(@PathVariable Long pageId) {
+        return ResponseEntity.ok(pageService.detail(pageId));
     }
 
     @PutMapping("/modfiy")
+    @ApiOperation(value = "page 정보를 수정하기위한 API", notes = "page정보를 수정하기 위해, image, content, sequence, pageId가 필요하다.", response = PageDtoResponse.class)
     public ResponseEntity<PageDtoResponse> modify(@RequestBody PageDtoPutRequest pageDtoPutRequest){
         return ResponseEntity.ok(pageService.modify(pageDtoPutRequest));
     }
 
     @DeleteMapping("/delete/{pageId}")
+    @ApiOperation(value = "page 정보를 삭제하기 위한 API", notes = "page 정보를 삭제하기 위해 pageId가 필요하다.", response = Boolean.class)
     public ResponseEntity<Boolean> delete(@PathVariable Long pageId){
         pageService.delete(pageId);
         return ResponseEntity.ok(true);
