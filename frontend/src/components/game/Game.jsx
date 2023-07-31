a8398f531bdd4588ae3a10d1d65f0df6e0592874
daehyuk <sdh5645@gmail.com>
2023-07-31 13:43:58 +0900

    Feat: 게임화면

--- frontend/src/components/game/Game.jsx
+++ frontend/src/components/game/Game.jsx
@@ -1,35 +1,39 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import './Game.css';
 
 export default function Game() {
   return (
     <>
-      <Box sx={{ display: "flex" }}>
+      <Box sx={{ display: 'flex', width: '100%', height: '100%' }}>
         <Box
           sx={{
-            display: "flex",
-            width: "50%",
-            height: "100%",
-            bgcolor: "#CCD5AE",
-            borderRadius: "61px",
-            justifyContent: "center",
-            alignItems: "center",
-            boxShadow: 5,
+            display: 'flex',
+            width: '100%',
+            height: '100%',
+            justifyContent: 'center',
+            alignItems: 'center',
           }}
-        />
-        <Box
-          sx={{
-            display: "flex",
-            width: "50%",
-            height: "100%",
-            flexGrow: 1,
-            bgcolor: "#CCD5AE",
-            borderRadius: "61px",
-            justifyContent: "center",
-            alignItems: "center",
-            boxShadow: 5,
-          }}
-        ></Box>
+        >
+          <main class="page-content">
+            <div class="card">
+              <div class="content">
+                <h2 class="title">시작하기</h2>
+                <p class="copy">
+                  Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains
+                </p>
+                <button class="btn">View Trips</button>
+              </div>
+            </div>
+            <div class="card">
+              <div class="content">
+                <h2 class="title">방만들기</h2>
+                <p class="copy">Plan your next beach trip with these fabulous destinations</p>
+                <button class="btn">View Trips</button>
+              </div>
+            </div>
+          </main>
+        </Box>
       </Box>
     </>
   );
--- frontend/src/page/Main.jsx
+++ frontend/src/page/Main.jsx
@@ -1,26 +1,22 @@
-import * as React from "react";
-import PropTypes from "prop-types";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
-import Nav from "../layouts/Nav";
-import DialButton from "../layouts/DialButton";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Nav from '../layouts/Nav';
+import DialButton from '../layouts/DialButton';
 
 function Main() {
   return (
     <>
       <Box
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          width: "100%",
-          height: "100%",
-          bgcolor: "#CCD5AE",
-          borderRadius: "61px",
-          justifyContent: "center",
-          alignItems: "center",
-          boxShadow: 20,
+          display: 'flex',
+          flexDirection: 'column',
+          width: '90%',
+          height: '90%',
+          bgcolor: '#CCD5AE',
+          borderRadius: '40px',
+          justifyContent: 'center',
+          alignItems: 'center',
+          boxShadow: 5,
         }}
       >
         <Nav></Nav>
