b0ba1515d02cdeff32b816181f5c60b5bfd8c4bb
Kwoo9 <rhks1132ek@naver.com>
2023-08-01 14:50:10 +0900

    Feat Likes Swagger Likes에 swagger 추가

--- backend/src/main/java/com/kong/authtest/community/dto/CommunityDtoGetResponse.java
+++ backend/src/main/java/com/kong/authtest/community/dto/CommunityDtoGetResponse.java
@@ -2,6 +2,7 @@ package com.kong.authtest.community.dto;
 
 import com.kong.authtest.comment.dto.CommentDtoResponse;
 import com.kong.authtest.community.model.Community;
+import com.kong.authtest.likes.service.LikesService;
 import lombok.Data;
 
 import java.util.List;
@@ -13,12 +14,12 @@ public class CommunityDtoGetResponse {
     private String content;
     private String title;
     private List<CommentDtoResponse> commentList;
+    private Long likes;
 
     public CommunityDtoGetResponse(Community community) {
         this.title = community.getTitle();
         this.content = community.getContent().getContent();
         this.communityId = community.getCommunityId();
         this.commentList = community.getCommentList().stream().map(CommentDtoResponse::new).collect(Collectors.toList());
-
     }
 }
