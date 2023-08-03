ea7d0a5bc0270dcb1d3b10aadc5e7b59bd7a9a23
박범진 <bumjin4u@naver.com>
2023-08-03 16:08:54 +0900

    Feat: Axios작업 및 마이페이지 좋아요 페이지 생성

---
+++ frontend/src/api/auth/Put/CommentAxios.jsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+async function CommentAxios(props) {
+  const headers = {
+    "Content-Type": "application/json", // JSON 형식으로 데이터를 보낼 것이므로 'application/json' 설정
+    // 추가적인 헤더 필요시 여기에 추가
+  };
+  try {
+    await axios.put(
+      "//i9c110.p.ssafy.io/api/comment/modify",
+      {
+        commentId: props.commentId,
+        content: props.content,
+      },
+      headers
+    );
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export default CommentAxios;
