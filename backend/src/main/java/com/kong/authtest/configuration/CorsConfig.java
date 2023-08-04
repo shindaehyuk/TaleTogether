a2a914c59531adbcb88330a6f37be16c0f4c8b06
Kong-16 <alsgur97@naver.com>
2023-08-04 18:01:18 +0900

    Fix Cors Errors Cors 에러 해결을 위한 CorsConfig 파일 수정 및 dockerfile 수정

--- backend/src/main/java/com/kong/authtest/configuration/CorsConfig.java
+++ backend/src/main/java/com/kong/authtest/configuration/CorsConfig.java
@@ -8,6 +8,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+//        서버에서 돌릴 땐 주석 해제해야합니다. 로컬 사용을 위해 주석처리.
 //        registry.addMapping("/**")
 //                .allowedOrigins("https://i9c110.p.ssafy.io")
 //                .allowedMethods("*")
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './redux/store/store';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
-import registerServiceWorker from './registerServiceWorker';
 
 const persistor = persistStore(store);
 
@@ -23,5 +21,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-registerServiceWorker();
+// reportWebVitals();
+// registerServiceWorker();
