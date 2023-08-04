ba3717032f92e5618c746c55e543951f476aab08
daehyuk <sdh5645@gmail.com>
2023-08-04 14:03:03 +0900

    update

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Main from '../page/Main';
 import Mypage from '../components/mypage/Mypage';
 import Community from '../components/community/Community';
-import PostDetail from '../components/community/Postdetail'
+import PostDetail from '../components/community/Postdetail';
 import Admin from '../page/Admin';
 
 import PrivateRoute from '../components/route/PrivateRoute';
@@ -10,6 +10,7 @@ import PublicRoute from '../components/route/PublicRoute';
 // import VideoRoomComponent from '../webRTC/VideoRoomComponent';
 import Game from '../components/game/Game';
 import Intro from '../components/main/Intro';
+import NotFound from '../page/NotFound';
 
 function Router() {
   return (
@@ -28,6 +29,7 @@ function Router() {
         <Route Component={PublicRoute}>
           <Route path="/" Component={Admin} />
         </Route>
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </>
   );
