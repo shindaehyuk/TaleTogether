ca350c65b77a9e177523d138dc7e78bc24b0ff8b
Jang Tae Won <xodnjs1837@gmail.com>
2023-08-07 15:44:33 +0900

    Fix : Tale Dto 수정

--- backend/src/main/java/com/kong/authtest/tale/dto/TaleDtoGetResponse.java
+++ backend/src/main/java/com/kong/authtest/tale/dto/TaleDtoGetResponse.java
@@ -10,11 +10,16 @@ import java.util.stream.Collectors;
 @Data
 public class TaleDtoGetResponse {
     private Long taleId;
+    private String title;
+
+    private String titleImage;
     private List<PageDtoResponse> PageList;
 
 
     public TaleDtoGetResponse(Tale tale) {
         this.taleId = tale.getTaleId();
+        this.title = tale.getTitle();
+        this.titleImage = tale.getTitleImage();
         this.PageList = tale.getPageList().stream().map(PageDtoResponse::new).collect(Collectors.toList());
     }
 }
