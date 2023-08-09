55a7487e4f16c409bda549c1c2d7d94f13e91d32
daehyuk <sdh5645@gmail.com>
2023-08-09 21:44:56 +0900

    Feat: 로그인토큰으로 변경

--- frontend/src/api/chat-gpt/createChatAxios.jsx
+++ frontend/src/api/chat-gpt/createChatAxios.jsx
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
 export default async function createChatAxios(props) {
+  console.log(props);
   try {
     // POST 요청은 body에 실어 보냄
     const res = await axios.post(
-      // '//i9c110.p.ssafy.io/api/create-chat',
-      'http://localhost:8083/api/create-chat',
+      '//i9c110.p.ssafy.io/api/create-chat',
+      // 'http://localhost:8083/api/create-chat',
       {
         chatGptRequest: {
           messages: [
@@ -13,15 +14,19 @@ export default async function createChatAxios(props) {
               role: 'system',
               content: 'You are a helpful assistant.',
             },
+            {
+              role: 'user',
+              content: props.content,
+            },
           ],
         },
         userChoiceRequest: {
-          backGround: '산',
-          player1: '범진',
-          player2: '범진2',
-          player1Character: '용감한',
-          player2Character: '소심한',
-          turn: '50',
+          backGround: props.backGround,
+          player1: props.player1,
+          player2: props.player2,
+          player1Character: props.player1Character,
+          player2Character: props.player2Character,
+          turn: props.turn,
         },
         pageDtoRequest: {
           taleId: 1,
