50f59223edbd4134385440e02086df67879102b2
daehyuk <sdh5645@gmail.com>
2023-08-11 14:50:55 +0900

    Feat:openvidu서버

--- frontend/src/api/gameroom/makeGameAxios.jsx
+++ frontend/src/api/gameroom/makeGameAxios.jsx
@@ -4,14 +4,14 @@ export default async function makeGameAxios(props) {
   const userId = sessionStorage.getItem('email');
   try {
     // POST 요청은 body에 실어 보냄
-    const res = await axios.get(
-      // '//i9c110.p.ssafy.io/api/register-game',
-      `http://localhost:8083/api/register-game`,
+    const res = await axios.post(
+      '//i9c110.p.ssafy.io/openvidu/api/sessions',
+      // `http://localhost:8083/api/register-game`,
       {},
       {
         headers: {
           'Content-Type': 'application/json',
-          Authorization: sessionStorage.getItem('token'),
+          Authorization: `Basic ${btoa('OPENVIDUAPP:THEATERGLOATE')}`,
         },
       }
     );
