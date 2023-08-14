67db43aabe4fb354536f6c238a4bdcda00075e19
Kwoo9 <rhks1132ek@naver.com>
2023-08-14 13:41:54 +0900

    Update AuthController
    userId 대신 token을 decode해서 사용하도록 변경

--- backend/src/main/java/com/kong/authtest/auth/controller/AuthController.java
+++ backend/src/main/java/com/kong/authtest/auth/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.kong.authtest.user.dto.UserDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -25,19 +26,18 @@ public class AuthController {
         return ResponseEntity.badRequest().body("토큰이 null");
     }
 
-    @GetMapping("/refresh/{userId}")
-    public ResponseEntity<?> refresh(HttpServletRequest request, @PathVariable final String userId) {
-        HashMap<String, Object> token = authService.refresh(request.getHeader(JwtTokenUtil.HEADER_STRING), userId);
+    @GetMapping("/refresh")
+    public ResponseEntity<?> refresh(HttpServletRequest request, final Authentication authentication) {
+        HashMap<String, Object> token = authService.refresh(request.getHeader(JwtTokenUtil.HEADER_STRING), (String) authentication.getPrincipal());
         if (token != null)
             return ResponseEntity.ok().body(token);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("token이 refresh안됨");
     }
 
-    @GetMapping("/logout/{userId}")
-    public ResponseEntity<?> logout(HttpServletRequest request, @PathVariable final String userId) {
-        authService.logout(request.getHeader(JwtTokenUtil.HEADER_STRING), userId);
+    @GetMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request, final Authentication authentication) {
+        authService.logout(request.getHeader(JwtTokenUtil.HEADER_STRING), (String) authentication.getPrincipal());
         return ResponseEntity.ok().build();
-
     }
 
 }
